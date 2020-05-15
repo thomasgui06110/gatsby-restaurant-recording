1d1e2c6b15d0079d6781fa31bf802bd04ee67690
thomasgui <guilhem.thomas@gmail.com>
2020-05-15 19:15:39 +0200

    fin

---
+++ src/components/HomePageComponents/Menu.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Product from "./Product";
+import { StaticQuery, graphql } from "gatsby";
+import { Link } from "gatsby";
+import { Section } from "../../utils";
+import Title from "../../utils/Title";
+import styled from "styled-components";
+
+const PRODUCTS = graphql`
+  {
+    items: allContentfulMenu {
+      edges {
+        node {
+          id
+          name
+          price
+          ingredients
+          img {
+            fixed(width: 150) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function Menu() {
+  return (
+    <Section>
+      <Title title="feature items" message="little taste" />
+      <ProductList>
+        <StaticQuery
+          query={PRODUCTS}
+          render={(data) => {
+            const products = data.items.edges;
+            return products.map((item) => {
+              return <Product key={item.node.id} product={item.node} />;
+            });
+          }}
+        />
+      </ProductList>
+    </Section>
+  );
+}
+
+const ProductList = styled.div`
+  margin: 3rem 0;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-row-gap: 3rem;
+  @media (min-width: 576px) {
+    grid-template-columns: 95%;
+  }
+  @media (min-width: 776px) {
+    grid-template-columns: 80%;
+    justify-content: center;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 2rem;
+  }
+`;
